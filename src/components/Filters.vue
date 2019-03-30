cc4c8be614537f32f6692006a50bc6ca657ee43b
IgnacioSan22 <iceamanos1998@gmail.com>
2019-03-30 17:36:34 +0100

    base de los filtros con boton mostrar más

--- src/components/Filters.vue
+++ src/components/Filters.vue
@@ -1,10 +1,10 @@
 <template>
-  <div role="tablist">
+  <div style="margin-top: 30px">
     <b-card no-body class="mb-1">
       <b-card-header header-tag="header" class="p-1" role="tab">
         <b-button block href="#" v-b-toggle.accordion-1 variant="info">Valoración media del vendedor</b-button>
       </b-card-header>
-      <b-collapse id="accordion-1" visible accordion="my-accordion" role="tabpanel">
+      <b-collapse id="accordion-1" visible accordion="my-accordion">
         <b-card-body>
           <b-card-text>I start opened because</b-card-text>
           <b-form-group>
@@ -35,19 +35,17 @@
     </b-card>
 
     <b-card no-body class="mb-1">
-      <b-card-header header-tag="header" class="p-1" role="tab">
+      <b-card-header header-tag="header" class="p-1">
         <b-button block href="#" v-b-toggle.accordion-2 variant="info">Precio</b-button>
       </b-card-header>
       <b-collapse id="accordion-2" accordion="my-accordion" role="tabpanel">
-        <b-card-body>
-          <vue-slide-bar>
-          </vue-slide-bar>
-        </b-card-body>
+        <VueSlideBar style="width: auto" v-model="value">
+        </VueSlideBar>
       </b-collapse>
     </b-card>
 
     <b-card no-body class="mb-1">
-      <b-card-header header-tag="header" class="p-1" role="tab">
+      <b-card-header header-tag="header" class="p-1" >
         <b-button block href="#" v-b-toggle.accordion-3 variant="info">Categorías</b-button>
       </b-card-header>
       <b-collapse id="accordion-3" accordion="my-accordion" role="tabpanel">
@@ -72,7 +70,10 @@
         text: 'Esto es un mensaje',
         optionsCat: [{text: 'Option 1 ', value: '1'},
           {text: 'Option 2 ', value: '2'},
-          {text: 'Option 3 ', value: '3'}]
+          {text: 'Option 3 ', value: '3'},
+          {text: 'Option 4 ', value: '4'},
+          {text: 'Option 5 ', value: '5'},],
+        value: 0
       }
     }
   }
--- src/components/RadioGroup.vue
+++ src/components/RadioGroup.vue
@@ -1,20 +1,34 @@
 <template>
+  <div>
   <b-form-group
     :label="title"
     label-for="basicCheckboxes"
     :label-cols="3"
     vertical>
-    <b-form-checkbox-group stacked id="basicCheckboxes" name="Checkboxes" :plain="true" :checked="[2,3]">
-      <b-form-checkbox v-for="option in options" :value="option.value" :key="option.value">{{option.text}}</b-form-checkbox>
+    <b-form-checkbox-group stacked id="basicCheckboxes" name="Checkboxes" :plain="true">
+      <b-form-checkbox v-if="show || option.value < 4" v-for="option in options" :value="option.value" :key="option.value" >{{option.text}}</b-form-checkbox>
     </b-form-checkbox-group>
   </b-form-group>
+    <b-button class="btn-link" v-on:click="showMore()">Mostrar más</b-button>
+  </div>
 </template>
 
 <script>
     export default {
         name: "RadioGroup",
+      data(){
+          return{
+            show: false
+          }
+      },
       props: {options: Array,
-            title: String}
+              title: String
+      },
+      methods: {
+         showMore :function () {
+           this.show = !this.show;
+         }
+      }
     }
 </script>
 
