8d22d1e83cb23ffdf72ff93464096aef2ddd552d
LÓPEZ FRANCO, MIGUEL JOSÉ <719436@celes.unizar.es>
2019-04-15 20:42:19 +0200

    Cambiado LogIn/SignIn por Acceder, traducciones varias

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -11,33 +11,36 @@
 
       <b-navbar-nav>
         <b-nav-item v-if="!isLogged" style="outline-color: green">
-          <router-link to="LogIn">Log In</router-link>
+          <router-link to="LogIn">Acceder</router-link>
         </b-nav-item>
-        <b-nav-item v-if="!isLogged">
-          <router-link to="Sign">Sing Up</router-link>
-        </b-nav-item>
+        <!--<b-nav-item v-if="!isLogged" style="outline-color: green">-->
+          <!--<router-link to="LogIn">Log In</router-link>-->
+        <!--</b-nav-item>-->
+        <!--<b-nav-item v-if="!isLogged">-->
+          <!--<router-link to="Sign">Sing Up</router-link>-->
+        <!--</b-nav-item>-->
         <b-nav-item>
-          <router-link to="Search">Search</router-link>
+          <router-link to="Search">Búsqueda</router-link>
         </b-nav-item>
       </b-navbar-nav>
 
       <!-- Right aligned nav items -->
       <b-navbar-nav class="ml-auto">
           <b-nav-form>
-            <b-form-input size="sm" class="mr-sm-2" type="text" placeholder="Search"/>
-            <b-button size="sm" class="my-2 my-sm-0" type="submit">Search</b-button>
+            <b-form-input size="sm" class="mr-sm-2" type="text" placeholder="Búsqueda"/>
+            <b-button size="sm" class="my-2 my-sm-0" type="submit">Buscar</b-button>
           </b-nav-form>
 
           <b-nav-item-dropdown right>
             <!-- Using button-content slot -->
             <template slot="button-content">
-              <em>User</em>
+              <em>Usuario</em>
             </template>
             <b-dropdown-item href="#/Profile">
               <!--<router-link to="Profile">Perfil</router-link>-->Perfil
             </b-dropdown-item>
-            <b-dropdown-item href="#">Settings</b-dropdown-item>
-            <b-dropdown-item v-on:click="logout">Signout</b-dropdown-item>
+            <b-dropdown-item href="#">Ajustes</b-dropdown-item>
+            <b-dropdown-item v-on:click="logout">Salir</b-dropdown-item>
           </b-nav-item-dropdown>
 
         <b-nav-item>
